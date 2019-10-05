5efa4b7907684bff0d3c947420965d80fda42896
chimera0108 <chimera0108@gmail.com>
2019-10-04 19:13:03 -0600

    Part 9 Comments in C#

---
+++ B009_Comments/B009_Comments/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace B009_Comments
+{
+    class Program
+    {
+        static void Main()
+        {
+            //Single Line Comment
+            //Console.WriteLine("Hello");
+
+            //Multiple Line Comments
+            /*
+            Console.WriteLine("Hello");
+            Console.WriteLine("Hello");
+            */
+
+            /*
+             *use the "Designer" buttons to comment/uncomment lines that are highlighted
+             * Good when doing multiple lines
+             * 
+             * ctrl K, ctrl C to comment the highlighted lines
+             * ctrl K, ctrl U to uncomment the highlighted lines
+             */
+            Console.WriteLine("Hello");
+            Console.WriteLine("Hello");
+
+            //Uncomment and hover over it to see the comment that was added to that class
+            //SampleClass
+            #region Keep Window Open
+            Console.ReadLine();
+            #endregion
+
+        }
+    }
+
+    /// <summary>
+    /// This is a sample class and a sample documentation
+    /// </summary>
+    public class SampleClass
+    {
+
+    }
+}
