acbbf5cd0f1bf5c12e59c2e5f2b7189b2d8c00ae
chimera0108 <chimera0108@gmail.com>
2019-10-08 17:03:30 -0600

    Part 13 While Loop in C#

---
+++ B013_Loops/B013_Loops/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace B013_Loops
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("Please enter your target number ");
+            int UserTarget = int.Parse(Console.ReadLine());
+
+            int Start = 0;
+            while(Start <= UserTarget)
+            {
+                Console.Write(Start + " ");
+                Start = Start + 2;
+            }
+            Console.WriteLine();
+
+            #region Keep Window Open
+            Console.ReadLine();
+            #endregion
+
+        }
+    }
+}
