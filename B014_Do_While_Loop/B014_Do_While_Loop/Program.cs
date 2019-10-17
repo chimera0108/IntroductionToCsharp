14c9f8d6ad8c0f606e572d8f9b3bf3ed406b3ba9
chimera0108 <chimera0108@gmail.com>
2019-10-17 14:15:04 -0600

    Part 14 Do While Loops

---
+++ B014_Do_While_Loop/B014_Do_While_Loop/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+
+namespace B014_Do_While_Loop
+{
+    class Program
+    {
+        static void Main()
+        {
+            string UserChoice = "Y";
+
+            while (UserChoice == "Y")
+            {
+                Console.WriteLine("Please enter your target? ");
+                int UserTarget = int.Parse(Console.ReadLine());
+
+                int Start = 0;
+            
+                while (Start <= UserTarget)
+                {
+                    Console.Write(Start + " ");
+                    Start = Start + 2;
+                }
+                Console.WriteLine();
+
+                do
+                {
+                    Console.WriteLine("Do you want to continue - Yes or No?");
+                    UserChoice = Console.ReadLine().ToUpper();
+
+                    switch (UserChoice)
+                    {
+                        case "YES":
+                        case "Y":
+                            UserChoice = "Y";
+                            break;
+                        case "NO":
+                        case "N":
+                            UserChoice = "N";
+                            break;
+                        default:
+                            Console.WriteLine("Your choice {0} is invalid. Please try again...", UserChoice);
+                            break;
+                    }
+                } while (UserChoice != "Y" && UserChoice != "N");
+            }
+
+
+            #region Keep Window Open
+            Console.ReadLine();
+            #endregion
+        }
+    }
+}
